75e77f3093c2764da18f2fbcaea9174305d8ec06
callumbirks <callumbirks@protonmail.com>
2021-02-22 10:51:34 +0000

    Add property tests to TstGames Add properties to clsGame Rename UnitTest1.cs to TstGames.cs

--- ClassLibrary/clsGame.cs
+++ ClassLibrary/clsGame.cs
@@ -1,6 +1,14 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsGame
     {
+        public int GameId { get; set; }
+        public string GameTitle { get; set; }
+        public double Price { get; set; }
+        public int Discount { get; set; }
+        public DateTime DatePublished { get; set; }
+        public bool Active { get; set; }
     }
 }
