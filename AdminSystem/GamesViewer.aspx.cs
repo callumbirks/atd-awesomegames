db510f2fa43903cbccd07b356bf546a17853bbcb
callumbirks <callumbirks@protonmail.com>
2021-02-22 14:07:09 +0000

    Add Games data entry page Add output to GamesViewer page for data entry input

--- AdminSystem/GamesViewer.aspx.cs
+++ AdminSystem/GamesViewer.aspx.cs
@@ -4,11 +4,18 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _GamesViewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        clsGame aGame = (clsGame)Session["aGame"];
+        Response.Write(aGame.GameId);
+        Response.Write(aGame.GameTitle);
+        Response.Write(aGame.Price);
+        Response.Write(aGame.Discount);
+        Response.Write(aGame.DatePublished);
+        Response.Write(aGame.Active);
     }
 }
